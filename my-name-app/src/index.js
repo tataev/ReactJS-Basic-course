2949f863b5e5ebb561132a5683d55b06f38a11b7
Khizri Tataev <bodycat@gmail.com>
2022-01-31 22:39:49 +0300

    Merge remote-tracking branch 'origin/hw04' into hw04

--- my-name-app/src/index.js
+++ my-name-app/src/index.js
@@ -2,7 +2,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import React,{useState, memo, useEffect, useRef, useState} from "react";
+// import React,{useState, memo, useEffect, useRef, useState} from "react";
+import React from "react";
 
 ReactDOM.render(
     <BrowserRouter>
